dc96c84aed3fd48cd99fcdc7b9118b4de6f2423d
ayissi joel <ayissi.nke.joel@gmail.com>
2020-10-17 15:57:51 +0200

    First commit

---
+++ app/src/main/java/com/turorial/ecommerce/MainActivity.java
@@ -0,0 +1,30 @@
+package com.turorial.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button loginButton , joinNowButton ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.already_have_account_btn);
+        joinNowButton = findViewById(R.id.join_now);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void layoutInitialisation(){
+
+    }
+}
